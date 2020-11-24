a1c246615dccf0d0b4dbc336d04fabd17398d596
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-11-23 21:14:06 -0600

    A necessary evil

--- src/main/java/io/github/thecursedfabricproject/cursedfluidapi/ItemFluidExtractable.java
+++ src/main/java/io/github/thecursedfabricproject/cursedfluidapi/ItemFluidExtractable.java
@@ -1,8 +1,13 @@
 package io.github.thecursedfabricproject.cursedfluidapi;
 
+import com.mojang.datafixers.util.Pair;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public interface ItemFluidExtractable {
     public Identifier getFluidKey();
-    public long extractFluidAmount(long maxamount, Identifier fluidKey);
+    public @Nullable Pair<ItemStack, Long> extractFluidAmount(long maxamount, Identifier fluidKey);
 }
--- src/main/java/io/github/thecursedfabricproject/cursedfluidapi/ItemFluidInsertable.java
+++ src/main/java/io/github/thecursedfabricproject/cursedfluidapi/ItemFluidInsertable.java
@@ -1,7 +1,12 @@
 package io.github.thecursedfabricproject.cursedfluidapi;
 
+import com.mojang.datafixers.util.Pair;
+
+import org.jetbrains.annotations.Nullable;
+
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public interface ItemFluidInsertable {
-    public long insertFluid(long amount, Identifier fluidkey);
+    public @Nullable Pair<ItemStack, Long> insertFluid(long amount, Identifier fluidkey);
 }
