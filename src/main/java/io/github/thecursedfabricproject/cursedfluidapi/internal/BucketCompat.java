b11502e43dc0c11e965c9f6c325321c1d4a82bab
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-11-26 20:03:57 -0600

    Improve Bucket Compat

--- src/main/java/io/github/thecursedfabricproject/cursedfluidapi/internal/BucketCompat.java
+++ src/main/java/io/github/thecursedfabricproject/cursedfluidapi/internal/BucketCompat.java
@@ -2,6 +2,7 @@ package io.github.thecursedfabricproject.cursedfluidapi.internal;
 
 import io.github.thecursedfabricproject.cursedfluidapi.FluidApiKeys;
 import io.github.thecursedfabricproject.cursedfluidapi.FluidConstants;
+import io.github.thecursedfabricproject.cursedfluidapi.FluidExtractable;
 import io.github.thecursedfabricproject.cursedfluidapi.FluidInsertable;
 import io.github.thecursedfabricproject.cursedfluidapi.FluidView;
 import io.github.thecursedfabricproject.cursedfluidapi.mixin.BucketItemAccess;
@@ -32,6 +33,35 @@ public class BucketCompat {
                 @Override
                 public long getFluidAmount(int slot) {
                     return getFluid(0) == Fluids.EMPTY ? 0 : FluidConstants.BUCKET;
+                }
+    
+            };
+        });
+
+        FluidApiKeys.ITEM_FLUID_EXTRACTABLE.registerFallback((stack, context) -> {
+            if (!(stack.getItem() instanceof BucketItem)) return null;
+            return new FluidExtractable() {
+                @Override
+                public int getSlotCount() {
+                    return 1;
+                }
+
+                @Override
+                public Fluid getFluid(int slot) {
+                    return ((BucketItemAccess) stack.getItem()).getFluid();
+                }
+    
+                @Override
+                public long getFluidAmount(int slot) {
+                    return getFluid(0) == Fluids.EMPTY ? 0 : FluidConstants.BUCKET;
+                }
+
+                @Override
+                public long extractFluidAmount(long maxamount, Fluid fluid, boolean simulation) {
+                    Fluid selfFluid = getFluid(0);
+                    if (!selfFluid.equals(fluid) || selfFluid.equals(Fluids.EMPTY)) return 0;
+                    if (!simulation) context.setMainStack(new ItemStack(Items.BUCKET));
+                    return FluidConstants.BUCKET;
                 }
     
             };
